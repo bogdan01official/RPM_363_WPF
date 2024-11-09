3970ef49581f0a0ccd02b25001a2aed98eb61a65
bogdan01official <108180181+bogdan01official@users.noreply.github.com>
2024-11-09 17:48:24 +0500

    Add files via upload

---
+++ 363_RPM_WPF/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private int NumderNow = 0;
+
+        private void addNum(object sender, MouseButtonEventArgs e)
+        {
+            NumList.Items.Add(NumderNow);
+            NumderNow++;
+        }
+
+        private void delNum(object sender, MouseButtonEventArgs e)
+        {
+           if (NumList.SelectedItem != null) {
+                NumList.Items.Remove(NumList.SelectedItem);
+            }
+        }
+
+        private void clearNumList(object sender, MouseButtonEventArgs e)
+        {
+            NumList.Items.Clear();
+            NumderNow = 0;
+        }
+    }
+}
